5d5f5181f87d409ccec9d5eace56cfcb9c3efb3b
RomainCharpentier <romaincharpentier10@gmail.com>
2020-02-04 18:01:48 +0100

    Updates

---
+++ precache-manifest.57de03d18dabbdb3dae83f6f2836b9d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5624015d1b95b1be785168a1644bde9",
+    "url": "/My-Resume/index.html"
+  },
+  {
+    "revision": "6b1701d62319f4e7d8d9",
+    "url": "/My-Resume/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e12be0f49c230f4bc87b",
+    "url": "/My-Resume/static/js/2.17fe7fa6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/My-Resume/static/js/2.17fe7fa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b1701d62319f4e7d8d9",
+    "url": "/My-Resume/static/js/main.147f0cf4.chunk.js"
+  },
+  {
+    "revision": "143498387e13a2fa9e12",
+    "url": "/My-Resume/static/js/runtime-main.e992458c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/My-Resume/static/media/logo.5d5d9eef.svg"
+  }
+]);
