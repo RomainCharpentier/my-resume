71994d3e9b447deef2aa53df05e05ffe4b90ce55
Romain <romain.charpentier10@gmail.com>
2021-04-26 10:45:31 +0200

    Correctif sens slice dans ShowMoreText

--- src/components/ShowMoreText.js
+++ src/components/ShowMoreText.js
@@ -2,7 +2,7 @@ import React, { useState, useMemo } from 'react';
 import { Typography, Button } from '@material-ui/core';
 
 const ShowMoreText = ({ textArray, numberOfLines }) => {
-    const [text, setText] = useState(textArray.slice(textArray.length - numberOfLines));
+    const [text, setText] = useState(textArray.slice(0, numberOfLines));
     // eslint-disable-next-line react-hooks/exhaustive-deps
     const displayButton = useMemo(() => text.length !== textArray.length, [textArray]);
 
@@ -10,10 +10,10 @@ const ShowMoreText = ({ textArray, numberOfLines }) => {
         if (text.length === numberOfLines) {
             setText(textArray);
         } else {
-            setText(textArray.slice(textArray.length - numberOfLines));
+            setText(textArray.slice(0, numberOfLines));
         }
     }
-    //TODO: Mauvais sens de slice
+    
     return(
         <div>
             {text.map((line, index) => <Typography variant='body2' key={index}>{line}</Typography>)}
